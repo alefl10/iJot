86c86462a0e7b32b1fd0c0c8ea0d8441ee2a0b72
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-01-15 00:58:00 -0500

    Added server side form validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import hb from 'express-handlebars';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import './models/Idea';
 
 const app = express();
@@ -16,6 +17,10 @@ const Idea = mongoose.model('ideas');
 // Handlebars Middleware
 app.engine('handlebars', hb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Body Parser Middleware + Parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Index Route
 app.get('/', (req, res) => {
@@ -33,6 +38,27 @@ app.get('/about', (req, res) => {
 // Add Idea form
 app.get('/ideas/add', (req, res) => {
 	res.render('ideas/add');
+});
+
+// Process Idea form
+app.post('/ideas/', (req, res) => {
+	const errors = [];
+
+	if (!req.body.title) {
+		errors.push({ text: 'Please add a title' });
+	}
+	if (!req.body.details) {
+		errors.push({ text: 'Please add some details' });
+	}
+	if (errors.length > 0) {
+		res.render('ideas/add', {
+			errors,
+			title: req.body.title,
+			details: req.body.details,
+		});
+	} else {
+		res.send('passed');
+	}
 });
 
 const port = 5000;
