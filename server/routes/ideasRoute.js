7d5402549f3107ff1f24490d3470bdb8dcb83ee0
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-01-31 00:10:04 -0500

    Protected routes based on user authentication

--- server/routes/ideasRoute.js
+++ server/routes/ideasRoute.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import controller from '../controllers/ideasController';
+import { ensureAuthenticated } from '../../helpers/auth';
 
 const router = express.Router();
 
 router.route('/')
-	.get(controller.get)
-	.post(controller.post);
+	.get(ensureAuthenticated, controller.get)
+	.post(ensureAuthenticated, controller.post);
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
 	res.render('ideas/add');
 });
 
-router.get('/edit/:id', controller.getOne);
+router.get('/edit/:id', ensureAuthenticated, controller.getOne);
 
 // Edit Form process
 router.route('/:id')
-	.put(controller.put)
-	.delete(controller.delete);
+	.put(ensureAuthenticated, controller.put)
+	.delete(ensureAuthenticated, controller.delete);
 
 export default router;
